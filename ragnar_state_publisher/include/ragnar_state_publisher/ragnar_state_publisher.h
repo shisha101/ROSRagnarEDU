9d67333365996053642e6f5bae716bdefe3b8d21
agoins <alex.goins@swri.org>
2015-09-21 08:43:02 -0500

    Fixed visualization of upper links

--- ragnar_state_publisher/include/ragnar_state_publisher/ragnar_state_publisher.h
+++ ragnar_state_publisher/include/ragnar_state_publisher/ragnar_state_publisher.h
@@ -7,6 +7,7 @@
 #include <string>
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
+//#include "ragnar_kinematics/ragnar_kinematics.h"
 
 namespace ragnar_state_publisher
 {
@@ -18,10 +19,14 @@ public:
 
   void updateJointPosition(const sensor_msgs::JointStateConstPtr& joints);
 
+  //void calculateDirectedTransform(const Eigen::Vector3f& start, const Eigen::Vector3f& stop, tf::Transform& transform);
 private:
   ros::NodeHandle nh_;
   tf::TransformBroadcaster tf_broadcaster_;
   ros::Subscriber joint_sub_;
+
+  tf::Transform base_transform_;
+  std::vector<tf::Vector3> zi_;
 };
 
 }
--- ragnar_state_publisher/src/ragnar_state_publisher.cpp
+++ ragnar_state_publisher/src/ragnar_state_publisher.cpp
@@ -1,5 +1,7 @@
 #include "ragnar_state_publisher/ragnar_state_publisher.h"
 #include "ragnar_kinematics/ragnar_kinematics.h"
+#include "tf/transform_listener.h"
+#include "ragnar_kinematics/ragnar_kinematic_defs.h"
 
 static tf::Vector3 toTF(const Eigen::Vector3f& v)
 {
@@ -8,6 +10,7 @@ static tf::Vector3 toTF(const Eigen::Vector3f& v)
 
 static void calculateDirectedTransform(const Eigen::Vector3f& start,
                                        const Eigen::Vector3f& stop,
+                                       const tf::Vector3 z_axis,
                                        tf::Transform& transform)
 {
   tf::Vector3 origin;
@@ -21,8 +24,7 @@ static void calculateDirectedTransform(const Eigen::Vector3f& start,
 
   tf::Vector3 n = toTF(stop) - toTF(start);
   n.normalize();
-  tf::Vector3 z;
-  z.setZ(1.0);
+  tf::Vector3 z = z_axis;
   tf::Vector3 y;
   y = n.cross(z);
   y.normalize();
@@ -40,15 +42,18 @@ static
 void calculateLinkTransforms(const Eigen::Vector3f& a,
                              const Eigen::Vector3f& b,
                              const Eigen::Vector3f& c,
+                             const tf::Vector3 z_axis,
                              tf::Transform& upper_tf,
                              tf::Transform& lower_tf)
 {
-  calculateDirectedTransform(a, b, upper_tf);
-  calculateDirectedTransform(b, c, lower_tf);
+  calculateDirectedTransform(a, b, z_axis, upper_tf);
+  calculateDirectedTransform(b, c, z_axis, lower_tf);
 }
 
 
 namespace rsp = ragnar_state_publisher;
+
+
 
 rsp::RagnarStatePublisher::RagnarStatePublisher(const std::string& joints_topic)
 {
@@ -57,6 +62,27 @@ rsp::RagnarStatePublisher::RagnarStatePublisher(const std::string& joints_topic)
                       boost::bind(&rsp::RagnarStatePublisher::updateJointPosition,
                                   this,
                                   _1));
+  tf::Matrix3x3 mat;
+  mat.setEulerYPR(-RAGNAR_JOINT1_BASE_PAN, 0, -RAGNAR_JOINT1_BASE_TILT);
+  zi_.push_back(mat.getColumn(2));
+  mat.setEulerYPR(-RAGNAR_JOINT2_BASE_PAN, 0, -RAGNAR_JOINT2_BASE_TILT);
+  zi_.push_back(mat.getColumn(2));
+  mat.setEulerYPR(-RAGNAR_JOINT3_BASE_PAN, 0, -RAGNAR_JOINT3_BASE_TILT);
+  zi_.push_back(mat.getColumn(2));
+  mat.setEulerYPR(-RAGNAR_JOINT4_BASE_PAN, 0, -RAGNAR_JOINT4_BASE_TILT);
+  zi_.push_back(mat.getColumn(2));
+
+  /*
+  tf::TransformListener listener;
+  ros::Duration(0.5).sleep();
+  if(listener.waitForTransform("base_link2","base_link", ros::Time::now(), ros::Duration(3.0)))
+  {
+    listener.lookupTransform("base_link2","base_link",ros::Time::now(),base_transform_);
+  }
+  else
+  {
+    ROS_WARN("Could not find transform from base_link to base_link2");
+  }*/
 }
 
 void rsp::RagnarStatePublisher::updateJointPosition(const sensor_msgs::JointStateConstPtr& joints)
@@ -82,7 +108,7 @@ void rsp::RagnarStatePublisher::updateJointPosition(const sensor_msgs::JointStat
 
   tf::Transform upper_link, lower_link;
   // Joint 1
-  calculateLinkTransforms(pts.A.col(0), pts.B.col(0), pts.C.col(0), upper_link, lower_link);
+  calculateLinkTransforms(pts.A.col(0), pts.B.col(0), pts.C.col(0), zi_[0], upper_link, lower_link);
   tf_broadcaster_.sendTransform(tf::StampedTransform(upper_link,
                                                      joints->header.stamp,
                                                      "base_link",
@@ -90,9 +116,9 @@ void rsp::RagnarStatePublisher::updateJointPosition(const sensor_msgs::JointStat
   tf_broadcaster_.sendTransform(tf::StampedTransform(lower_link,
                                                      joints->header.stamp,
                                                      "base_link",
-                                                     "lower_arm_1"));
+                                                     "lower_arm_1_yaw"));
   // Joint 2
-  calculateLinkTransforms(pts.A.col(1), pts.B.col(1), pts.C.col(1), upper_link, lower_link);
+  calculateLinkTransforms(pts.A.col(1), pts.B.col(1), pts.C.col(1), zi_[1], upper_link, lower_link);
   tf_broadcaster_.sendTransform(tf::StampedTransform(upper_link,
                                                      joints->header.stamp,
                                                      "base_link",
@@ -100,9 +126,9 @@ void rsp::RagnarStatePublisher::updateJointPosition(const sensor_msgs::JointStat
   tf_broadcaster_.sendTransform(tf::StampedTransform(lower_link,
                                                      joints->header.stamp,
                                                      "base_link",
-                                                     "lower_arm_2"));
+                                                     "lower_arm_2_yaw"));
   // Joint 3
-  calculateLinkTransforms(pts.A.col(2), pts.B.col(2), pts.C.col(2), upper_link, lower_link);
+  calculateLinkTransforms(pts.A.col(2), pts.B.col(2), pts.C.col(2), zi_[2], upper_link, lower_link);
   tf_broadcaster_.sendTransform(tf::StampedTransform(upper_link,
                                                      joints->header.stamp,
                                                      "base_link",
@@ -110,9 +136,9 @@ void rsp::RagnarStatePublisher::updateJointPosition(const sensor_msgs::JointStat
   tf_broadcaster_.sendTransform(tf::StampedTransform(lower_link,
                                                      joints->header.stamp,
                                                      "base_link",
-                                                     "lower_arm_3"));
+                                                     "lower_arm_3_yaw"));
   // Joint 4
-  calculateLinkTransforms(pts.A.col(3), pts.B.col(3), pts.C.col(3), upper_link, lower_link);
+  calculateLinkTransforms(pts.A.col(3), pts.B.col(3), pts.C.col(3), zi_[3], upper_link, lower_link);
   tf_broadcaster_.sendTransform(tf::StampedTransform(upper_link,
                                                      joints->header.stamp,
                                                      "base_link",
@@ -120,7 +146,7 @@ void rsp::RagnarStatePublisher::updateJointPosition(const sensor_msgs::JointStat
   tf_broadcaster_.sendTransform(tf::StampedTransform(lower_link,
                                                      joints->header.stamp,
                                                      "base_link",
-                                                     "lower_arm_4"));
+                                                     "lower_arm_4_yaw"));
   // world -> base_link
   tf::Transform world_tf = tf::Transform::getIdentity();
   tf_broadcaster_.sendTransform(tf::StampedTransform(world_tf,
