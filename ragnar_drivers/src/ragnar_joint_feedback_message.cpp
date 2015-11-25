b68257789ba86281198cd07da39ec186b4fce522
Jonathan Meyer <jmeyer@swri.org>
2015-11-24 19:53:08 -0600

    Added joint names to the feedback publisher. We really need to decide on a standard naming convention for the joints.

--- ragnar_drivers/src/ragnar_joint_feedback_message.cpp
+++ ragnar_drivers/src/ragnar_joint_feedback_message.cpp
@@ -7,6 +7,15 @@ namespace ragnar_drivers
 {
 using namespace industrial;
 
+// Add Joint Names
+static void addJointNames(sensor_msgs::JointState& msg)
+{
+  msg.name.push_back("ragnar_joint1");
+  msg.name.push_back("ragnar_joint2");
+  msg.name.push_back("ragnar_joint3");
+  msg.name.push_back("ragnar_joint4");
+}
+
 // Message handler
 RagnarJointFeedbackHandler::RagnarJointFeedbackHandler()
 {
@@ -18,6 +27,9 @@ bool RagnarJointFeedbackHandler::internalCB(simple_message::SimpleMessage& in)
 {
   sensor_msgs::JointState msg;
   msg.header.stamp = ros::Time::now();
+  msg.header.frame_id = "base_link";
+
+  addJointNames(msg);
 
   joint_feedback_message::JointFeedbackMessage joint_msg;
   joint_msg.init(in);
